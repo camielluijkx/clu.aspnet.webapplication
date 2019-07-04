9c538c39d25a0aa6dcc69ff8d0e8b428b26966f1
Camiel Luijkx <camiel.luijkx@gmail.com>
2019-07-04 18:51:31 +0200

    custom controller factory

---
+++ clu.aspnet.webapplication.mvc/Controllers/GeneralPurposeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace clu.aspnet.webapplication.mvc.Controllers
+{
+    public class GeneralPurposeController : Controller
+    {
+        // GET: GeneralPurpose
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ clu.aspnet.webapplication.mvc/Controllers/PhotoController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace clu.aspnet.webapplication.mvc.Controllers
+{
+    public class PhotoController : Controller
+    {
+        // GET: Photo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
