466f9512987004584ba2610fe0193a8f477eb585
Camiel Luijkx <camiel.luijkx@gmail.com>
2019-07-04 19:00:13 +0200

    custom filter action attribute

---
+++ clu.aspnet.webapplication.mvc/Attributes/SimpleActionFilter.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using System.Web.Mvc;
+
+namespace clu.aspnet.webapplication.mvc.Attributes
+{
+    public class SimpleActionFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            Debug.WriteLine("This Event Fired: OnActionExecuting");
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            Debug.WriteLine("This Event Fired: OnActionExecuted");
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            Debug.WriteLine("This Event Fired: OnResultExecuting");
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            Debug.WriteLine("This Event Fired: OnResultExecuted");
+        }
+    }
+}
--- clu.aspnet.webapplication.mvc/Controllers/GeneralPurposeController.cs
+++ clu.aspnet.webapplication.mvc/Controllers/GeneralPurposeController.cs
@@ -1,10 +1,12 @@
-﻿using System.Web.Mvc;
+﻿using clu.aspnet.webapplication.mvc.Attributes;
+using System.Web.Mvc;
 
 namespace clu.aspnet.webapplication.mvc.Controllers
 {
     public class GeneralPurposeController : Controller
     {
         // GET: GeneralPurpose
+        [SimpleActionFilter]
         public ActionResult Index()
         {
             return View();
