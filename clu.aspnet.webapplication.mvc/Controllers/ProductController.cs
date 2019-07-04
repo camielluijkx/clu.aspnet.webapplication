1c3a8fc51879ca388f5ea88483a0b642ef6fad1f
Camiel Luijkx <camiel.luijkx@gmail.com>
2019-07-04 19:51:05 +0200

    controller exception handling

--- clu.aspnet.webapplication.mvc/Controllers/ProductController.cs
+++ clu.aspnet.webapplication.mvc/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using clu.aspnet.webapplication.mvc.Models;
 using clu.aspnet.webapplication.mvc.Repository;
+using System;
 using System.Collections.Generic;
 using System.Web.Mvc;
 
@@ -24,11 +25,34 @@ namespace clu.aspnet.webapplication.mvc.Controllers
         }
 
         //Add action methods here
+        [HandleError(ExceptionType = typeof(NotImplementedException), View = "NotImplemented")]
+        [HandleError]
         public ActionResult Index()
         {
             var model = new List<Product>();
 
             return View(model);
+        }
+
+        protected override void OnException(ExceptionContext filterContext)
+        {
+            var controllerName = filterContext.RouteData.Values["controller"].ToString();
+            var actionName = filterContext.RouteData.Values["action"].ToString();
+
+            // Catch invalid operation exception
+            if (filterContext.Exception is InvalidOperationException)
+            {
+                var model = new HandleErrorInfo(filterContext.Exception, controllerName, actionName);
+                var result = new ViewResult
+                {
+                    ViewName = "Error",
+                    ViewData = new ViewDataDictionary<HandleErrorInfo>(model),
+                    //Save pass the current Temp Data to the Error view, because it often contains
+                    //diagnostic information
+                    TempData = filterContext.Controller.TempData
+                };
+                filterContext.Result = result;
+            }
         }
     }
 }
