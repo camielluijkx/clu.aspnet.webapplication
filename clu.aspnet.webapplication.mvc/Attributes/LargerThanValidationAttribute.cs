4d42f7483ca745dc9f3489dfd9351f73d929eb84
Camiel Luijkx <camiel.luijkx@gmail.com>
2019-07-04 18:37:11 +0200

    custom validation attribute

---
+++ clu.aspnet.webapplication.mvc/Attributes/LargerThanValidationAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace clu.aspnet.webapplication.mvc.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class LargerThanValidation : ValidationAttribute
+    {
+        public int MinimumValue { get; set; }
+
+        //Constructor
+        public LargerThanValidation(int minimum)
+        {
+            MinimumValue = minimum;
+        }
+
+        //You must override the IsValid method to run your test
+        public override Boolean IsValid(Object value)
+        {
+            var valueToCompare = (int)value;
+            if (valueToCompare > MinimumValue)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ clu.aspnet.webapplication.mvc/Models/Person.cs
@@ -0,0 +1,10 @@
+﻿using clu.aspnet.webapplication.mvc.Attributes;
+
+namespace clu.aspnet.webapplication.mvc.Models
+{
+    public class Person
+    {
+        [LargerThanValidation(18)]
+        public int VoterAge { get; set; }
+    }
+}
