91b8219c9b499abf112b8663062a3418ccb64a35
Camiel Luijkx <cluijkx@netaspect.nl>
2019-07-26 16:18:22 +0200

    added .net core mvc web application

---
+++ clu.aspnet.webapplication.mvc.core/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace clu.aspnet.webapplication.mvc.core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
