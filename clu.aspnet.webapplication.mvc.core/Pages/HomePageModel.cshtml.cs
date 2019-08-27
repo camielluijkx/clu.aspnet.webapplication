419f1d9ecbdd3f2a922aeef87324eacf3f1a7a8d
Camiel Luijkx <camiel.luijkx@gmail.com>
2019-08-27 10:19:42 +0200

    summary for module 1.3

---
+++ clu.aspnet.webapplication.mvc.core/Pages/HomePageModel.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace clu.aspnet.webapplication.mvc.core.Pages
+{
+    public class HomePageModel : PageModel
+    {
+        public string Description { get; set; }
+        public string MoreInfo { get; set; }
+
+        public void OnGet()
+        {
+            Description = "Your application description is here.";
+            MoreInfo = "Your application extra information is here.";
+        }
+    }
+}
