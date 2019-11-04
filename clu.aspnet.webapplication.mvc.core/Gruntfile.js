d1f81ee4be5e4c7f91c8e0916ca35d343ed43403
Camiel Luijkx <cluijkx@netaspect.nl>
2019-11-04 12:27:38 +0100

    summary for module 9.2 (incomplete)

---
+++ clu.aspnet.webapplication.mvc.core/Gruntfile.js
@@ -0,0 +1,17 @@
+﻿module.exports = function (grunt) {
+    grunt.initConfig({
+        sass: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'Styles',
+                    src: ['**/*.scss'],
+                    dest: 'wwwroot/styles',
+                    ext: '.css'
+                }]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-sass");
+};
