d47df7a4a8bc9c9b5d674c6c20202b366641d8fa
Camiel Luijkx <cluijkx@netaspect.nl>
2019-11-04 10:49:32 +0100

    summary for moudle 9.1

---
+++ clu.aspnet.webapplication.mvc.core/Controllers/BootstrapControler.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace clu.aspnet.webapplication.mvc.core.Controllers
+{
+    public class BootstrapController : Controller
+    {
+        public IActionResult Alerts()
+        {
+            return View();
+        }
+
+        public IActionResult Buttons()
+        {
+            return View();
+        }
+
+        public IActionResult Dropdowns()
+        {
+            return View();
+        }
+
+        public IActionResult Forms()
+        {
+            return View();
+        }
+
+        public IActionResult Navs()
+        {
+            return View();
+        }
+
+        public IActionResult Navbars()
+        {
+            return View();
+        }
+
+        public IActionResult Modals()
+        {
+            return View();
+        }
+    }
+}
